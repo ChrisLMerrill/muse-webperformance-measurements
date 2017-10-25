9f9c742ec709ffdcad1c6396d55d1dedbe27e349
Chris <chris@webperformance.com>
2017-10-25 17:10:25 -0400

    Initialization of DataCollectors now happens via a ContextInitializerConfiguration provided by muse-core.

--- src/main/kotlin/com/webperformance/muse/measurements/StepDurationCollector.kt
+++ src/main/kotlin/com/webperformance/muse/measurements/StepDurationCollector.kt
@@ -5,6 +5,7 @@ import org.musetest.core.MuseEvent
 import org.musetest.core.MuseEventListener
 import org.musetest.core.MuseEventType
 import org.musetest.core.MuseExecutionContext
+import org.musetest.core.context.initializers.ContextInitializerConfiguration
 import org.musetest.core.datacollection.DataCollector
 import org.musetest.core.events.StepEvent
 import org.musetest.core.step.StepConfiguration
@@ -18,42 +19,52 @@ private val logger = KotlinLogging.logger {}
  * @author Christopher L Merrill (see LICENSE.txt for license details)
  */
 class StepDurationCollector : MuseEventListener, DataCollector
-	{
+{
 	private val startTime = HashMap<StepConfiguration, Long>()
 	private val data = StepDurations()
-
+	
+	override fun configure(configuration: ContextInitializerConfiguration?)
+	{
+		// not expecting any configuration parameters yet
+	}
+	
+	override fun getType(): String
+	{
+		return "com.webperformance.step-durations"
+	}
+	
 	override fun initialize(context: MuseExecutionContext)
-		{
+	{
 		context.addEventListener(this)
-		}
-
+	}
+	
 	override fun getData(): StepDurations
-		{
+	{
 		return data
-		}
-
+	}
+	
 	override fun eventRaised(event: MuseEvent)
-		{
+	{
 		if (event.type == MuseEventType.StartStep)
-			{
+		{
 			val start = event as StepEvent
 			startTime.put(start.config, start.timestampNanos)
-			}
+		}
 		else if (event.type == MuseEventType.EndStep)
-			{
+		{
 			val end = event as StepEvent
-			val started : Long? = startTime.remove(end.config)
+			val started: Long? = startTime.remove(end.config)
 			if (started != null)
-				{
+			{
 				val measurement = Measurement(end.timestampNanos - started)
 				data.durations.add(measurement)
-				}
-			}
-		else if (event.type == MuseEventType.EndTest)
-			{
-			data.durations.iterator().forEach(operation = { measurement -> logger.error("measured: ${measurement.value}") })
 			}
 		}
+		else if (event.type == MuseEventType.EndTest)
+		{
+			data.durations.iterator().forEach(operation = { measurement -> logger.error("measured: ${measurement.value}") })
+		}
 	}
+}
 
 
